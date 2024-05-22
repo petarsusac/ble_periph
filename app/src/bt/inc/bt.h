0ae6d49b857b94716d9a8db222c8496fab54e608
Petar Susac <petar.susac@fer.hr>
2024-05-22 15:00:09 +0200

    Decouple BT module

---
+++ app/src/bt/inc/bt.h
@@ -0,0 +1,12 @@
+#ifndef _BT_H_
+#define _BT_H_
+
+#include <stdint.h>
+#include <stddef.h>
+
+typedef void (*bt_connected_cb_t)(void);
+
+int bt_start(bt_connected_cb_t conn_cb);
+int bt_send_notification(uint8_t *data, size_t len);
+
+#endif /* _BT_H_ */
